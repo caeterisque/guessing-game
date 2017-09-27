c50c291bbeff4aa8bc052febf0336c2c096f94e2
Ekaterina Sigaeva <sigaeva.katyusha@mail.ru>
2017-09-27 16:27:01 +0300

    first commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min
+      this.max = max
     }
 
     guess() {
-
-    }
+      return (this.min + Math.ceil((this.max - this.min)/2))
+      }
 
     lower() {
-
+      this.max = this.min + Math.ceil((this.max - this.min)/2)
     }
 
     greater() {
-
+      this.min = this.min + Math.ceil((this.max - this.min)/2)
     }
 }
 
